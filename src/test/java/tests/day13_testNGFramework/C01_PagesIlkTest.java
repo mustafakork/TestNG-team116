1cf7a149f06cc31c4c0f85d65a7466bc54cdf51c
mustafakork <mstfbsrysrykb60@gmail.com>
2023-07-10 23:58:59 +0300

    day12

---
+++ src/test/java/tests/day13_testNGFramework/C01_PagesIlkTest.java
@@ -0,0 +1,21 @@
+package tests.day13_testNGFramework;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utulities.Driver;
+
+public class C01_PagesIlkTest {
+
+    @Test
+    public void amazonTest(){
+
+        //amazon anasayfaya gidin
+        //sonuçların nutella içerdiğini test edin
+        //sayfayı kapatın
+        Driver.getDriver().get("https://amazon.com");
+        AmazonPage amazonPage =new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("Nutella"+ Keys.+);
+
+    }
+}
