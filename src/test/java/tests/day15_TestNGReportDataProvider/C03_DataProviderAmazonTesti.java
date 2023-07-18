6a4ff230cf05e0edf1cf56413752bc61617eff84
mustafakork <mstfbsrysrykb60@gmail.com>
2023-07-18 16:17:10 +0300

    day15

--- src/test/java/tests/day15_TestNGReportDataProvider/C03_DataProviderAmazonTesti.java
+++ src/test/java/tests/day15_TestNGReportDataProvider/C03_DataProviderAmazonTesti.java
@@ -1,7 +1,11 @@
 package tests.day15_TestNGReportDataProvider;
 
+import org.openqa.selenium.Keys;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class C03_DataProviderAmazonTesti {
 
@@ -9,12 +13,26 @@ public class C03_DataProviderAmazonTesti {
     public static Object[][] AranacakUrunListesi() {
        //data provider notasyonuna sahip metodlar çift katlı aray
         //döndürmek zorundadır
+
+
         String[][] urunListesi={{"Nutella"},{"Cokokrem"},{"java"},{"Selenium"}};
         return urunListesi;
     }
 
     @Test(dataProvider = "AranacakUrunListesi")
-    public void cokluAramaTesti() {
+    public void cokluAramaTesti(String aranacakurun) {
+
+        //amazon anasayfasına gidin
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        //verilen listedki her bir ürün için ayrı ayrı
+        //aramaypıpp, sonuçların o ürünü ,çerdiğini test edin
+        //Nutella, çokokrem,Java,selenium, Apple, Samsung
+
+        AmazonPage amazonPage =new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys(aranacakurun+ Keys.ENTER);
+
+        Driver.closeDriver();
 
 
     }
--- src/test/java/tests/day15_TestNGReportDataProvider/C04_DataProviderNegatifLoginTesti.java
+++ src/test/java/tests/day15_TestNGReportDataProvider/C04_DataProviderNegatifLoginTesti.java
@@ -1,11 +1,55 @@
 package tests.day15_TestNGReportDataProvider;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import pages.AmazonPage;
+import pages.QDemyPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
 
-public class C04_DataProviderNegatifLoginTesti {
+public class C04_DataProviderNegatifLoginTesti extends TestBaseRapor {
 
-    @Test
-    public void cokluNegatifLoginTesti(String kullanıcıada,String pasword){
+    @DataProvider
+    public static Object[][] kullanıcıListesi() {
+
+        String[][] kullanıcıİsimŞifreleri= {{"sevda","12345"},
+                                            {"mustafa", "12345"},
+                                            {"ahmet","45789"}};
+        return kullanıcıİsimŞifreleri;
+    }
+
+    @Test(dataProvider = "kullanıcıListesi")
+    public void cokluNegatifLoginTesti(String kullanıcıadı,String pasword){
+        extentTest=extentReports.createTest("Data provider","qdm test");
+
+        //qualitydemy anasayfasına gidin
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        extentTest.info("qdm ana  sayfaya gider");
+
+
+        //liste olarak verilen kullanıcı adı  ve şifreleri ile giriş yapılamadığını test edin
+
+        //ilk login linkine tıklayın
+        QDemyPage qDemyPage =new QDemyPage();
+        qDemyPage.cookies.click();
+        extentTest.info("cookies tıklar");
+        qDemyPage.ılklogin.click();
+        extentTest.info("login butonuna basar");
+                qDemyPage.emailloginElementi.click();
+                extentTest.info("kullanıcı" + kullanıcıadı+"email girer");
+        //kullanıcı adı e şifresini yazıpgiriş yapmayı deneyin
+        qDemyPage.emailloginElementi.sendKeys(kullanıcıadı);
+        qDemyPage.paswordElementi.sendKeys(pasword);
+        qDemyPage.ikinciloginElementi.click();
+
+        // giriş yapılamadığını test edin
+        Assert.assertTrue(qDemyPage.emailloginElementi.isDisplayed());
+        extentTest.pass("sayfaya girilemediği test eder");
+
+        //sayfayı kapatın
+        Driver.closeDriver();
 
 
     }
